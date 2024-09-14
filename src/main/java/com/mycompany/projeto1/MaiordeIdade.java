dea3307444fc92ccb6ed78333b124fa81dd0fcfa
SelmaMonteiro <selmamonteiro251@gmail.com>
2024-09-13 22:35:29 -0300

    java

---
+++ src/main/java/com/mycompany/projeto1/MaiordeIdade.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class MaiordeIdade {
+
+    public static void main(String[] args) {
+        //ENTRADA
+        
+        Scanner scanner = new Scanner(System.in);
+        int idade;
+        
+       //PROCESSAMENTO
+       idade = scanner.nextInt();
+       if(idade >= 18) {
+           System.out.println ("----- Programa de comparação maior de idade-----");
+           System.out.println ("Aluno maior de idade");
+
+       }
+
+       else{
+           System.out.println ("Aluno menor de idade");
+       }
+    }   
+}      
---
+++ src/main/java/com/mycompany/projeto1/Projeto_1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Projeto1 {
+
+    public static void main(String[] args) {
+        //ENTRADA
+        
+        int A,B,C,D;
+        A=1;
+        B=4;
+        C=8 ;
+        
+       //PROCESSWAMENTO
+       D=A+B+C;
+       
+       //SAÍDA
+       
+       System.out.println("***Treino de estrutura logica***");
+      System.out.println("o valor de D será:"+D);
+    }
+}
